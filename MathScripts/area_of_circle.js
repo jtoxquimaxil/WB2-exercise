a99c17d5102d2b73f8fd965c076f0cbc55cb6b2e
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-17 15:40:43 -0400

    found the area of the circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,14 @@
+"use strict"
+
+//Pie
+var Pie = 3.14;
+
+//radius to the second power
+var circleRadius = 11;
+
+// calculations of the area 
+var circleRadius = Pie * circleRadius;
+
+
+//The area of a circle with radius of some-number is some number
+console.log("The area of the cirlce is " + circleRadius);
---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,17 @@
+"use strict"
+
+//width
+var rectangleWidth = 8;
+
+
+//height
+var rectangleHeight = 20;
+
+
+//area calculations
+var rectangleArea = rectangleWidth * rectangleHeight;
+
+
+
+//The are of the rectangle is some number 
+console.log("The area of the rectangle is " + rectangleArea)
