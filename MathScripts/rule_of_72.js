8fc9c4b4ebfe10583bc7cfdec21601f9b37eade1
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-17 15:57:12 -0400

    completed rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,15 @@
+"use scripts"
+
+//divide 72
+var ruleNumber = 72;
+
+
+//Interest Rate 
+var interestRate = 3;
+
+//Calculations
+var yearstoDouble = ruleNumber / interestRate;
+
+
+//At a some-number% interest rate, your savings account will be worth some-number in some number years
+console.log("It will take" + yearstoDouble);
