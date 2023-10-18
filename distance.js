96dff2c06164fa9004209a2a5ea1bf16a3af25f5
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-18 12:31:16 -0400

    completed rentals.js

---
+++ distance.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// formula
+
+var X1 = 3;
+
+var X2 = -2;
+
+var Y1 = 7;
+
+var Y2 = 15;
+
+var step1 = Math.pow(-2 - 3);
+
+var step2 = Math.pow(15 - 7);
+
+var step3 = step1 + step2;
+
+console.log("The answer is " + step3);
+
---
+++ rentals.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// var passengers = 38;
+
+// var vanSeat = 15;
+
+// var vansNeeded = Math.ceil(passengers / vanSeat);
+
+// console.log("The number of vans needed are " + vansNeeded);
+
+// var vanCost = 250;
+
+// var totalVansCost = 3 + vanCost;
+
+// console.log("The total cost of the vans needed was " + totalVansCost);
+
+var passengers = 38;
+
+var chargePerPerson = 253 / passengers;
+
+console.log("Each person was charged " + chargePerPerson);
+
+
