57fe6d46da73d386406c6c24ed16cfc372804768
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-18 13:15:40 -0400

    completed distance.js

--- distance.js
+++ distance.js
@@ -2,19 +2,26 @@
 
 // formula
 
-var X1 = 3;
+// var X1 = 3;
 
-var X2 = -2;
+// var X2 = -2;
 
-var Y1 = 7;
+// var Y1 = 7;
 
-var Y2 = 15;
+// var Y2 = 15;
 
-var step1 = Math.pow(-2 - 3);
+// var step1 = -2 - 3;
 
-var step2 = Math.pow(15 - 7);
+// var step2 = 15 - 7;
 
-var step3 = step1 + step2;
+// console.log("The answer to step 1 is" + step1 + "The answer to step 2 is " + step2);
 
-console.log("The answer is " + step3);
+// var step3 = Math.pow(-5, 2);
 
+// var step4 = Math.pow(8, 2);
+
+// console.log("the root number of step three is " + step3 + "the root number of step four is " + step4  );
+
+var answer = Math.sqrt(25 + 64);
+
+console.log("the distance between X1,Y1 and Y1,Y2 is " + answer );
