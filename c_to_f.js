c88abde600bf1ac0211ea441cfc9d41e8be5d792
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-18 10:06:52 -0400

    completed c_to_f.js

---
+++ c_to_f.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// formula F=9/5C+32
+
+// Celcius to Fahrenheit
+
+var celsiusTemperature = 36
+
+var startingFormula = 9/5 
+
+var startingNumber = 32
+
+var nextStep = startingFormula * celsiusTemperature
+
+var fahrenheitTemperature = nextStep + startingNumber
+
+console.log("The Celcius temperature into fahrenheit is " + fahrenheitTemperature)
