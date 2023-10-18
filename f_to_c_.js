fbbebd84f2f8ef9b699ce3f44833d63fabfe86cd
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-18 10:03:11 -0400

    completed f_to_c

---
+++ f_to_c_.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// formiula is c=5/9(F-32)
+
+// Fahrenheit temperature
+var fahrenheitTemperature = 48;
+
+// formula to convert 
+var formulaToConvert = 5/9; 
+
+// Celsius temperature
+var celsiusTemperature = fahrenheitTemperature * formulaToConvert;
+
+console.log("Fahrenheit to celsius equals " + celsiusTemperature);
+
+
+
+
