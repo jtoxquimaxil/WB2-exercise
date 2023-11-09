2c293340371b1db4ac9114ada88326030c58bf5a
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-11-09 11:55:22 -0500

    completed show_major.js

---
+++ IfScripts/show_major.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+var studentName = "Jakelin";
+
+var studentMajorCode = "BIOL";
+
+var studentMajor;
+
+var advisingLocation;
+
+if (studentMajorCode == "CSCI") {
+    studentMajor = "Computer Science"
+    advisingLocation = "Shephard Hall, Room 311";
+} else if (studentMajorCode == "BIOL") {
+    studentMajor = "Biology"
+    advisingLocation = "Science Bldg, Room 310"
+}
+
+console.log(studentName);
+console.log(studentMajor);
+console.log(advisingLocation);
+
+
+////////////////
+
+
+var studentName = "Leilani";
+
+var studentMajorCode = "CSCI";
+
+var studentMajor;
+
+var advisingLocation;
+
+if (studentMajorCode == "CSCI") {
+    studentMajor = "Computer Science"
+    advisingLocation = "Shephard Hall, Room 311";
+} else if (studentMajorCode == "BIOL") {
+    studentMajor = "Biology"
+    advisingLocation = "Science Bldg, Room 310"
+}
+
+console.log(studentName);
+console.log(studentMajor);
+console.log(advisingLocation);
